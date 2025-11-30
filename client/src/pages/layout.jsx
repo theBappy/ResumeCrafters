13cf6c0b2f991c51694c956d8d60f0ecc5a5b205
theBappy <abappy575@gmail.com>
2025-11-30 12:31:15 +0600

    user dashboard ui

--- client/src/pages/layout.jsx
+++ client/src/pages/layout.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom"
+import Navbar from "../components/navbar"
 
 const LayoutPage = () => {
   return (
     <div>
-      <h1>Layout Page</h1>
-      <div className="text-black">
+      <div className="min-h-screen bg-gray-50">
+        <Navbar />
         <Outlet />
       </div>
     </div>
